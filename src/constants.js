cc83d98cd5409307c197b4e6186bcdb222d85ebc
link2x <link2x@users.noreply.github.com>
2023-07-05 13:38:43 -0400

    Categories (Prepared Foods)

--- src/constants.js
+++ src/constants.js
@@ -1,6 +1,6 @@
 const safetyRatings = ['unknown', 'safe', 'sometimes unsafe', 'usually unsafe', 'always unsafe', 'allergen', 'tooth sensitivity', 'sensory issue']
 
-const itemCategories = ['Vegetables', 'Meats', 'Spices', 'Condiments', 'Fruits', 'Preservatives', 'Dairy', 'Drinks', 'Grains']
+const itemCategories = ['Vegetables', 'Meats', 'Spices', 'Condiments', 'Fruits', 'Preservatives', 'Dairy', 'Drinks', 'Grains', 'Prepared Foods']
 
 const authErrors = {
   "auth/wrong-password": "The password is invalid or the user does not have a password.",
