8cf04a4ce6b14286cfcda095e738bd61b78f0bad
link2x <link2x@users.noreply.github.com>
2023-07-14 14:29:58 -0400

    Temp disable persistence

--- src/firebase.js
+++ src/firebase.js
@@ -25,16 +25,16 @@ const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 const auth = getAuth(app);
 
-// Enable offline persistence
-enableIndexedDbPersistence(db)
-  .catch((err) => {
-      if (err.code == 'failed-precondition') {
-          // Multiple tabs open, persistence can only be enabled
-          // in one tab at a a time.
-      } else if (err.code == 'unimplemented') {
-          // The current browser does not support all of the
-          // features required to enable persistence
-      }
-  });
+// // Enable offline persistence
+// enableIndexedDbPersistence(db)
+//   .catch((err) => {
+//       if (err.code == 'failed-precondition') {
+//           // Multiple tabs open, persistence can only be enabled
+//           // in one tab at a a time.
+//       } else if (err.code == 'unimplemented') {
+//           // The current browser does not support all of the
+//           // features required to enable persistence
+//       }
+//   });
 
 export {app, db, auth}
